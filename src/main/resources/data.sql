75f3a63102a94867698099a2a91634a21799b73d
yhh1056 <yhh1056@naver.com>
2022-06-08 21:57:03 +0900

    feat: 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,27 +1,26 @@
 insert into customer (account, nickname, password, address, phone_number)
-values ('puterism', 'hoho', '12345678', 'hohome', '01012345678'),
-       ('tanney-102', 'hoho', '12345678', 'hohome', '01012345678'),
-       ('jho2301', 'hoho', '12345678', 'hohome', '01012345678'),
-       ('365kim', 'hoho', '12345678', 'hohome', '01012345678'),
-       ('dudtjr913', 'hoho', '12345678', 'hohome', '01012345678'),
-       ('jum0', 'hoho', '12345678', 'hohome', '01012345678'),
-       ('hyuuunjukim', 'hoho', '12345678', 'hohome', '01012345678'),
-       ('zereight', 'hoho', '12345678', 'hohome', '01012345678'),
-       ('devhyun637', 'hoho', '12345678', 'hohome', '01012345678'),
-       ('swon3210', 'hoho', '12345678', 'hohome', '01012345678'),
-       ('bigsaigon333', 'hoho', '12345678', 'hohome', '01012345678'),
-       ('yungo1846', 'hoho', '12345678', 'hohome', '01012345678'),
-       ('zigsong', 'hoho', '12345678', 'hohome', '01012345678'),
-       ('iborymagic', 'hoho', '12345678', 'hohome', '01012345678'),
-       ('0307kwon', 'hoho', '12345678', 'hohome', '01012345678'),
-       ('gwangyeol-im', 'hoho', '12345678', 'hohome', '01012345678'),
-       ('shinsehantan', 'hoho', '12345678', 'hohome', '01012345678'),
-       ('ddongule', 'hoho', '12345678', 'hohome', '01012345678'),
-       ('seojihwan', 'hoho', '12345678', 'hohome', '01012345678'),
-       ('0imbean0', 'hoho', '12345678', 'hohome', '01012345678'),
-       ('sunyoungkwon', 'hoho', '12345678', 'hohome', '01012345678'),
-       ('hchayan', 'hoho', '12345678', 'hohome', '01012345678'),
-       ('2sooy', 'hoho', '12345678', 'hohome', '01012345678'),
-       ('yujo11', 'hoho', '12345678', 'hohome', '01012345678'),
-       ('sunhpark42', 'hoho', '12345678', 'hohome', '01012345678')
-;
+values ('hohohoho', 'eden', 'password123!', 'address', '01012345678');
+
+insert into product (name, price, image_url)
+values ('삼립호빵', 5000, 'https://m.bakingmon.com/web/product/big/20200604/ca6e09b022765e331140afd0f8f81cbc.jpg');
+
+insert into product (name, price, image_url)
+values ('마이쮸 복숭아', 500, 'http://www.thessan.com/shopimages/thessancom/0060020000042.jpg?1477553153');
+
+insert into product (name, price, image_url)
+values ('꼬깔콘', 1300, 'https://sitem.ssgcdn.com/05/90/32/item/0000008329005_i1_1200.jpg');
+
+insert into product (name, price, image_url)
+values ('[든든] 국내산 세척무 1개', 1750, 'https://cdn-mart.baemin.com/sellergoods/main/e2bd1641-7f1f-49ff-83df-8005a16d5e78.png');
+
+insert into product (name, price, image_url)
+values ('빅파이', 3000, 'http://www.thessan.com/shopimages/thessancom/0060020001432.jpg?1488531916');
+
+insert into product (name, price, image_url)
+values ('평양냉면', 5000, 'https://cdn-mart.baemin.com/sellergoods/bulk/20220519-162122/28147-main-01.jpg');
+
+insert into cart_item(customer_id, product_id)
+values (1, 1);
+
+insert into cart_item(customer_id, product_id)
+values (1, 2);
